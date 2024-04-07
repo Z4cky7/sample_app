1ff39b65bbf9a5183ab2c7aa4f59a06940f236ee
EC2 Default User <ec2-user@ip-172-31-28-121.ap-northeast-1.compute.internal>
2024-04-07 22:04:42 +0000

    Initial Commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,20 +8,29 @@ def create
 
   list.save
 
-  redirect_to '/top'
+  redirect_to list_path(list.id)
 end
 
   def index
      @lists = List.all
   end
-
   def show
+    @list = List.find(params[:id])
+
   end
 
   def edit
+    @list = List.find(params[:id])
   end
 
+ def update
+   list = List.find(params[:id])
+   list.update(list_params)
+   redirect_to list_path(list.id)
+ end
+ 
 private
+
 def list_params
   params.require(:list).permit(:title, :body)
 end
